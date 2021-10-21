21353dee8c6ccde17eda9643f7f11c115afe599b
sk534 <sk534@hotmail.com>
2021-10-22 01:13:03 +0530

    sample expense components

--- src/App.js
+++ src/App.js
@@ -3,11 +3,24 @@ import "./App.css";
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    { title: "Car Insurance", amount: 200.25, date: new Date(2021, 2, 28) },
+    { title: "Car Insurance1", amount: 300.25, date: new Date(2021, 1, 28) },
+  ];
   return (
     <div>
       <h2>Let's get started!</h2>
       <p>Sample text</p>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItem>
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,32 @@
-function ExpenseItem() {
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
+  // const expenseDate = new Date(2021, 2, 28);
+  // const expenseTitle = "Car Insurance";
+  // const expenseAmount = 200.25;
   return (
-    <div>
-      <h2>Expense Item</h2>
-      <p>Sample item here...</p>
+    <div className="expense-item">
+      {/* <div>{props.date.toISOString()}</div> */}
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <p className="expense-item__price">{props.amount}</p>
+      </div>
     </div>
+    // <div className="expense-item">
+    //   <div>{expenseDate.toISOString()}</div>
+    //   <div className="expense-item__description">
+    //     <h2>{expenseTitle}</h2>
+    //     <p className="expense-item__price">{expenseAmount}</p>
+    //   </div>
+    // </div>
   );
 }
 
