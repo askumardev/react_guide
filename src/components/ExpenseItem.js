0fcf122d8ef24861af5f4949d219d91ca53a5a55
Satish Kumar <sk534@hotmail.com>
2021-12-15 19:16:12 +0530

    first event hander

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,23 @@
-import React from 'react';
-import ExpenseDate from './ExpenseDate';
-import Card from './Card';
-import './ExpenseItem.css';
+import React from "react";
+import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
+import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-//function ExpenseItem(props) {
+  const cliclHandler = () => {
+    console.log("Clicked...");
+  };
+  //function ExpenseItem(props) {
   return (
-    <Card className='expense-item'>
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
-      <div className='expense-item__description'>
+      <div className="expense-item__description">
         <h2>{props.title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={cliclHandler}>Change Title</button>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
