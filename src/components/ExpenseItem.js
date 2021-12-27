1440b3c5118a97f6918c2c7abca4611eb549d429
Satish Kumar <sk534@hotmail.com>
2021-12-27 14:46:07 +0530

    introduction to state

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "./Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+  //  let title = props.title;
   const cliclHandler = () => {
-    console.log("Clicked...");
+    // title = "Updated...";
+    setTitle("Updated!!!");
+    console.log(title);
   };
   //function ExpenseItem(props) {
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={cliclHandler}>Change Title</button>
